c0a34f9917e34f91c92b1aef8d9f5af682636153
jurijjzz <jurijszuravlovs@gmail.com>
2022-12-01 19:36:22 +0100

    fixed error with vector

--- test.py
+++ test.py
@@ -2,12 +2,16 @@ import build.calculator
 from build.calculator import *
 
 print(dir(build.calculator))
-
+#Simple function
 print(sum(1.1,2))
 print(subs(3,2))
 print(mult(3,2))
 print(div(3,2))
+#Clases
 print("Class on")
 m = CalculatorClass(5)
 print(m.multiply(10))
-print(m.multiply_list(1,3,10))
+print(m.multiply_list([1,3,10]))
+
+#Class functions
+print(build.calculator.sum(2,2))
